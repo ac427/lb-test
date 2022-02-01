87a8299b3f983e682d82a8bfeed05722f1e7aa74
Ananta Chakravartula <ananta.c@gmail.com>
2022-02-01 18:04:46 -0500

    stdout response code in test

--- tests.sh
+++ tests.sh
@@ -5,12 +5,14 @@ set -e
 pip3 install --user pylint bottle
 
 pylint ./web/server.py
-docker build -t anantac/lb ./lb || travis_terminate 1
-docker build -t anantac/bottle ./web || travis_terminate 1
+docker build -t anantac/lb ./lb
+docker build -t anantac/bottle ./web
 /usr/local/bin/docker-compose up -d
 response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost/ping)
 
 if [[ $response -ne 200 ]];then
   echo 'health check failed'
   exit 1
+else
+  echo hurrah! health check passed. response code is - $response
 fi
