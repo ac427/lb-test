36ce7ecec4d428b2c3a89a6337802d9de3fc7fa7
Ananta Chakravartula <ananta.c@gmail.com>
2022-02-01 17:38:08 -0500

    added tests

---
+++ tests.sh
@@ -0,0 +1,12 @@
+set -e
+
+pylint ./web/server.py
+docker build -t anantac/lb ./lb || travis_terminate 1
+docker build -t anantac/bottle ./web || travis_terminate 1
+/usr/local/bin/docker-compose up -d
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost/ping)
+
+if [[ $response -ne 200 ]];then
+  echo 'health check failed'
+  exit 1
+fi
